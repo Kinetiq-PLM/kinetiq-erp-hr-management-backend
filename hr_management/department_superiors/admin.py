8ac95f356f758acd9f5ca3bb8476b6a1f8c79149
kaelthas <jmvl.academic@gmail.com>
2025-04-04 19:19:25 +0800

    Delete hr_management directory

--- hr_management/department_superiors/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import DepartmentSuperior
-
-@admin.register(DepartmentSuperior)
-class DepartmentSuperiorAdmin(admin.ModelAdmin):
-    list_display = ('dept_id', 'superior_job_title', 'hierarchy_level')
-    list_filter = ('hierarchy_level',)
-    search_fields = ('dept_id', 'superior_job_title')
-    readonly_fields = ('dept_id', 'superior_job_title')
--- hr_management/departments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import DepartmentListCreateAPIView, DepartmentDestroyAPIView
-
-urlpatterns = [
-    path('departments/', DepartmentListCreateAPIView.as_view(), name = 'department-list-create'),
-    path('departments/<str:dept_id>/', DepartmentDestroyAPIView.as_view(), name = 'department-delete'),
-]
