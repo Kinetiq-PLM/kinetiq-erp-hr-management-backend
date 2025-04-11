f6e10f709a8b82fe3523525e47265c3be26a2a92
kaelthas <jmvl.academic@gmail.com>
2025-04-11 19:56:30 +0800

    Delete department_superiors directory

--- department_superiors/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import DepartmentSuperior
-
-class DepartmentSuperiorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DepartmentSuperior
-        fields = '__all__'
-        read_only_fields = ('dept_id', 'superior_job_title')
