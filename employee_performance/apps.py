28d2849ba8cce3fa7962c3f1411e5f75d12270bb
kaelthas <jmvl.academic@gmail.com>
2025-04-11 19:56:49 +0800

    Delete employee_performance directory

--- employee_performance/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployeePerformanceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'employee_performance'
--- employee_performance/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import EmployeePerformance
-
-class EmployeePerformanceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EmployeePerformance
-        fields = '__all__'
