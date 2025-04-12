b8e61524e63774487521b84070ce8d2f9fc1f0b2
kaelthas <jmvl.academic@gmail.com>
2025-04-12 22:30:38 +0800

    Delete hrm_backend/employee_leave_requests directory
    For now.

--- hrm_backend/employee_leave_requests/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import LeaveRequest
-
-@admin.register(LeaveRequest)
-class LeaveRequestAdmin(admin.ModelAdmin):
-    list_display = ("leave_id", "employee", "leave_type", "start_date", "end_date", "status", "is_paid")
-    list_filter = ("leave_type", "status", "is_paid")
-    search_fields = ("employee__name", "leave_type", "status")
-    readonly_fields = ("created_at", "updated_at")
--- hrm_backend/employee_leave_requests/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployeeLeaveRequestsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'employee_leave_requests'
