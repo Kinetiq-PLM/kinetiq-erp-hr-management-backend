7235dc65b5770925d9ed5bf35c806133d0c2fa00
kaelthas <jmvl.academic@gmail.com>
2025-04-13 15:18:50 +0800

    Delete hrm_backend/employee_leave_balances/safe_copy directory

--- hrm_backend/employee_leave_balances/safe_copy/departments/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Department
-
-@admin.register(Department)
-class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ('dept_id', 'dept_name')
-    search_fields = ('dept_id', 'dept_name')
